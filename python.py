69ed5178645cec31791ada3f8bf6e690cff3dc50
Shankapotamus3 <154471660+Shankapotamus3@users.noreply.github.com>
2026-08-31 18:24:39 -0600

    Add files via upload

--- python.py
+++ python.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # Database
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dombot.db")
+DATABASE_URL = "sqlite:///./dombot.db")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
